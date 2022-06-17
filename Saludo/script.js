cb437e689695e8b2f40aa86b3d6f0781d3f96043
Lucia Cortes <luciacortesmolina@gmail.com>
2022-06-17 15:17:56 -0500

    se utiliza inner text para cambiar inner de h2

--- Saludo/script.js
+++ Saludo/script.js
@@ -3,16 +3,19 @@ console.log("Se nota que todavía me extrañas");
 
 setInterval(cambiarSaludo, 2000);
 
+/**
+ * Cambia la frase en el título h2
+ */
 function cambiarSaludo() {
   //console.log("Me parece una falta de respecto");
   let frase = document.getElementsByTagName("h2")[1].innerHTML;
 
   if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
     //frase = "Impongo moda para las niñas!";
-    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+    document.getElementsByTagName("h2")[1].innerHTML = "¡Impongo moda para las niñas!";
   } else {
     //frase = "Ganando como siempre";
-    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+    document.getElementsByTagName("h2")[1].innerHTML = "¡Ganando como siempre!";
   }
   console.log("frase :" + frase);
 }
